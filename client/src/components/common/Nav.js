187f4b2c8c41b8338ddc2b9f6f5d23b5a7dfa7f2
Weston <barnesweston@gmail.com>
2022-07-05 19:33:58 -0500

    add nav jsx

--- client/src/components/common/Nav.js
+++ client/src/components/common/Nav.js
@@ -7,6 +7,9 @@ import axios from 'axios';
 const Nav = () => {
     const [userInfo, setUserInfo] = useState([])
 
+    const handleClick = () => setClick(!click)
+    const closeMobileMenu = () => setClick(false)
+
     const userIsAuthenticated = () => {
         const payload = getPayload()
         if (!payload) return false
@@ -36,8 +39,43 @@ const Nav = () => {
                 <div className="nav-logo">
                     <Link to="/" className="nav-logo">Home</Link>
                 </div>
-                
+                <div className="menu-icon" onClick={handleClick}>
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                </div>
+                <ul className={click ? 'nav-menu-active': 'nav-menu'}>
+                    <li className="nav-item">
+                        <Link to="/wines" className="nav-links" onClick={closeMobileMenu}>
+                            Wines
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
+                            About
+                        </Link>
+                    </li>
+                    {!userIsAuthenticated() ?
+                    <>
+                    <li className="nav-item">
+                        <Link to="/register" className="nav-links" onClick={closeMobileMenu}>
+                            Sign Up
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/login" className="nav-links" onClick={closeMobileMenu}>
+                            Login
+                        </Link>
+                    </li>
+                    </>
+                    :
+                    <Link to="/profile">
+                        My profile
+                    </Link>
+                        
+                }
+                </ul>
             </nav>
         </div>
     )
 }
+
+export default Nav;
