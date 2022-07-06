c06bd6ff86d002223aeccd2cd2dcd45d0a3978de
Michael Cota <mikecotahf@gmail.com>
2022-07-05 20:03:35 -0400

    deleted suggestedwine.js

--- config/router.js
+++ config/router.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from "express";
 import {
   getAllWines,
@@ -8,28 +7,9 @@ import {
   deleteWine,
 } from "../controllers/shoppedWine.js";
 const router = express.Router();
-=======
-import express from 'express'
-import { getAllWines, createWine, displayWine, editWine, deleteWine } from '../controllers/wine.js'
-import { getAllUsers, getUser, createUser, deleteUser, editUser } from '../controllers/users.js'
-const router = express.Router()
->>>>>>> b0db4893fb5bfc28fbcc65218bf554a2461841a5
 
 router.route("/shoppedWine").get(getAllWines).post(createWine);
 
 router.route("/wines/:id").get(displayWine).put(editWine).delete(deleteWine);
 
-<<<<<<< HEAD
 export default router;
-=======
-router.route('/users')
-    .get(getAllUsers)
-    .post(createUser)
-
-router.route('/users/:id')
-    .get(getUser)
-    .put(editUser)
-    .delete(deleteUser)
-
-export default router
->>>>>>> b0db4893fb5bfc28fbcc65218bf554a2461841a5
