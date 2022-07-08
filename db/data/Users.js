b1d5f49476f90150ed1da812b5dbe000bdb49630
Weston <barnesweston@gmail.com>
2022-07-07 19:03:54 -0500

    add user data

--- db/data/Users.js
+++ db/data/Users.js
@@ -1,37 +1,44 @@
 export default [
-    {
-      username: '',
-      email: '',
-      password: '',
-      passwordConfirmation: '',
-      image: ''
-    },
-    {
-        username: '',
-        email: '',
-        password: '',
-        passwordConfirmation: '',
-        image: ''
-      },
-      {
-        username: '',
-        email: '',
-        password: '',
-        passwordConfirmation: '',
-        image: ''
-      },
-      {
-        username: '',
-        email: '',
-        password: '',
-        passwordConfirmation: '',
-        image: ''
-      },
-      {
-        username: '',
-        email: '',
-        password: '',
-        passwordConfirmation: '',
-        image: ''
-      },
+  {
+    username: 'Admin',
+    email: 'admin@email.com',
+    password: 'pass',
+    passwordConfirmation: 'pass',
+    image: ''
+  },
+  {
+    username: 'Test',
+    email: 'test@email.com',
+    password: 'pass',
+    passwordConfirmation: 'pass',
+    image: ''
+  },
+  {
+    username: 'Wes',
+    email: 'wes@email.com',
+    password: 'pass',
+    passwordConfirmation: 'pass',
+    image: ''
+  },
+  {
+    username: 'Mike',
+    email: 'mike@email.com',
+    password: 'pass',
+    passwordConfirmation: 'pass',
+    image: ''
+  },
+  {
+    username: 'Chen',
+    email: 'chen@email.com',
+    password: 'pass',
+    passwordConfirmation: 'pass',
+    image: ''
+  },
+  {
+    username: 'Abe',
+    email: 'abe@email.com',
+    password: 'pass',
+    passwordConfirmation: 'pass',
+    image: ''
+  },
     ]
