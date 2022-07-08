203baac69823984d23cc3c06537049b6e53f6f53
Weston <barnesweston@gmail.com>
2022-07-07 19:05:49 -0500

    add images to user data

--- db/data/Users.js
+++ db/data/Users.js
@@ -4,41 +4,41 @@ export default [
     email: 'admin@email.com',
     password: 'pass',
     passwordConfirmation: 'pass',
-    image: ''
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzurfDqrkxJ0raSXpjMFe85_c6z2guyzC41QrJ_LRYsMwYWNa-wzAG_N-woIwTYYyFtV0&usqp=CAU'
   },
   {
     username: 'Test',
     email: 'test@email.com',
     password: 'pass',
     passwordConfirmation: 'pass',
-    image: ''
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzurfDqrkxJ0raSXpjMFe85_c6z2guyzC41QrJ_LRYsMwYWNa-wzAG_N-woIwTYYyFtV0&usqp=CAU'
   },
   {
     username: 'Wes',
     email: 'wes@email.com',
     password: 'pass',
     passwordConfirmation: 'pass',
-    image: ''
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzurfDqrkxJ0raSXpjMFe85_c6z2guyzC41QrJ_LRYsMwYWNa-wzAG_N-woIwTYYyFtV0&usqp=CAU'
   },
   {
     username: 'Mike',
     email: 'mike@email.com',
     password: 'pass',
     passwordConfirmation: 'pass',
-    image: ''
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzurfDqrkxJ0raSXpjMFe85_c6z2guyzC41QrJ_LRYsMwYWNa-wzAG_N-woIwTYYyFtV0&usqp=CAU'
   },
   {
     username: 'Chen',
     email: 'chen@email.com',
     password: 'pass',
     passwordConfirmation: 'pass',
-    image: ''
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzurfDqrkxJ0raSXpjMFe85_c6z2guyzC41QrJ_LRYsMwYWNa-wzAG_N-woIwTYYyFtV0&usqp=CAU'
   },
   {
     username: 'Abe',
     email: 'abe@email.com',
     password: 'pass',
     passwordConfirmation: 'pass',
-    image: ''
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzurfDqrkxJ0raSXpjMFe85_c6z2guyzC41QrJ_LRYsMwYWNa-wzAG_N-woIwTYYyFtV0&usqp=CAU'
   },
     ]
