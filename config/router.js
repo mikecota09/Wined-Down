a834c5334b2ec14786dc105adc918e7b227b7270
Weston <barnesweston@gmail.com>
2022-07-07 19:27:47 -0500

    add controller to router

--- config/router.js
+++ config/router.js
@@ -5,7 +5,18 @@ import {
   displayShoppedWine,
   deleteShoppedWine,
 } from "../controllers/shoppedWine.js";
+import {
+  getAllWines,
+  getOneWine,
+  createWine,
+  editWine,
+  deleteWine
+} from '../controllers/wine.js'
+
 const router = express.Router();
+
+router.route('/wines').get(getAllWines).post(createWine);
+router.route('/wines/:id').get(getOneWine).put(editWine).delete(deleteWine);
 
 router.route("/shopped-wines").get(getAllShoppedWines);
 
